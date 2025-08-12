63581dca9da11f9da16ff32093c83fc2627c0585
Yantow25 <yantohamid77@gmail.com>
2025-08-12 18:52:43 +0800

    Update exp.js

--- assets/js/exp.js
+++ assets/js/exp.js
@@ -1,5 +1,6 @@
-const targetDate = new Date("2025-BULAN-TANGGALT23:59:59").getTime();
+const targetDate = new Date("2025-09-12T23:59:59").getTime();
         const currentDate = new Date().getTime();
         if (currentDate > targetDate) {
         window.location.href = "site.html";
+
         }
